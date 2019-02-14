0ac56631e8782c6d5e02fcf3e9cc2e84cef1dfd1
andrew schneider <aschne93@gmail.com>
2019-02-14 00:00:35 -0800

    added cloud function to insert data.
    - needs proper input response verification 
    - needs proper error checking/handling

--- main.py
+++ main.py
@@ -20,5 +20,35 @@ def postgres_demo(request):
             conn.close()
 
 
-results = postgres_demo(None)
-print(results)
+def insert_data(request):
+    # parse request data
+    request_json = request.get_json(silent=True)
+
+    if request_json:
+        sensor_id = request_json.get("sensor_id", None)
+        temperature = request_json.get("temperature", None)
+        moisture = request_json.get("moisture", None)
+
+    if None in [sensor_id, temperature, moisture]:
+        pass
+        # TODO should send back a 400 or something
+
+    conn = pg_connection(f"/cloudsql/{CONNECTION_NAME}")
+
+    try:
+        with conn.cursor() as cur:
+            insert = f"INSERT INTO sensor_data (sensor_id, temperature, moisture) values ({sensor_id}, {temperature}, {moisture});"
+            cur.execute(insert)
+            print(f"Inserting {sensor_id}, {temperature}, {moisture}")
+        conn.commit()
+        # TODO should send back a 201
+    # TODO should handle failure on commit and send back appropriate status code
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+
+if __name__ == "__main__":
+    results = postgres_demo(None)
+    print(results)
